a1476d6a770a2de43922307681e70d8498e6ce05
Nina Howley <ninahowley04@gmail.com>
2025-06-10 15:03:57 -0400

    testing

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import requests
+import csv
+from datetime import date
 
 def get_response(model, prompt, input):
     response = requests.post(
@@ -38,6 +40,11 @@ def get_response(model, prompt, input):
     )
     return response
 
+def reset_csv():
+    with open('query_tests.csv', 'w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['model', 'query','answer','response'])
+
 def test_bot(model, inputs, prompt):
         total = len(inputs)
         correct = 0
@@ -48,15 +55,19 @@ def test_bot(model, inputs, prompt):
             response = get_response(model, f"query: {input}", prompt).json()
             response = response['choices'][0]['message']['content'].split("\n")
 
-            print(response[-1])
             if "false" in response[-1].lower():
                 response = False
             else:
                 response = True
 
+            with open('query_tests.csv', 'a', newline='', encoding='utf-8') as outfile:
+                writer = csv.writer(outfile)
+                writer.writerow([model, input, inputs[input], response])
+
             print(f"Query: {statement}")
             print(f"Answer: {answer}")
-            print(f"Response: {response}", "\n")
+            print(f"Response: {response}")
+            print(response,"\n")
 
             if answer == response:
                  correct += 1
@@ -68,7 +79,20 @@ def test_bot(model, inputs, prompt):
         print(f"Percent correct: {percent_correct}%")
 
 model = "mistralai/mistral-7b-instruct-v0.3"
-inputs = {"Kamala Harris campaign news": True, "restaurants in Boston": False, "2024 election poll": True, "met gala worst looks": False}
+inputs = {
+        #true
+        "kamala harris campaign news": True, 
+        "2024 election poll": True, 
+        "who is running for president in 2028":True,
+        "impact of new immigration laws on economy":True,
+        "arguments for and against universal healthcare":True,
+        #false
+        "met gala worst looks": False,
+        "restaurants in boston": False,
+        "wellesley college academic calendar 2024-2025": False,
+        "latest advancements in quantum computing": False,
+        "removing coffee stains from carpet": False
+        }
 
 prompt = """
         You are an analyst who decided whether search queries are political in nature. 
@@ -77,4 +101,5 @@ prompt = """
         Respond strictly with one word: True or False (of whether the query is political).
         """
 
+reset_csv()
 test_bot(model, inputs, prompt)
